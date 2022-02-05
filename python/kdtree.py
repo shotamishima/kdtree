20c93c17529bdb0b79c1bb1b4fe97a9e5c16f116
shota-mishima <shota_mishima@toyota-tokyo.tech>
2022-02-06 02:40:33 +0900

    Refoctering

--- python/kdtree.py
+++ python/kdtree.py
@@ -3,21 +3,25 @@ from random import random
 
 class Node:
     def location(element):
-        self.location = element
+        location = element
 
     def lhs(point_list):
-        self.lhs = point_list
+        lhs = point_list
     
     def rhs(point_list):
-        self.rhs= point_list
+        rhs= point_list
 
 
 class kdtree:
 
-    # def __init__(self):
-    #     self.val = 0
+    def __init__(self, point_list):
+        self.point_list = point_list
+        self.node = Node()
 
-    def build(self, point_list, depth=0, leaf_size=4):
+    def build(self, depth=0, leaf_size=4):
+        self.node = self._build_recursive(self.point_list, depth, leaf_size)
+
+    def _build_recursive(self, point_list, depth=0, leaf_size=4):
         if not point_list:
             return
 
@@ -29,7 +33,7 @@ class kdtree:
         # axisでソート
         point_list.sort(key=lambda x: x[axis])
         # 中央値となるインデックスを算出
-        median = len(point_list) % 2
+        median = len(point_list) %2
         left_list = point_list[0: median]
         right_list = point_list[median + 1:]
 
@@ -37,22 +41,22 @@ class kdtree:
         if (len(left_list) > leaf_size) and (len(right_list) > leaf_size):
             node = Node()
             node.location = point_list[median] # その深さにおけるノードになる要素
-            node.lhs= self.build(point_list[0: median], depth + 1)
-            node.rhs =self.build(point_list[median + 1:], depth + 1)
+            node.lhs= self._build_recursive(point_list[0: median], depth + 1)
+            node.rhs =self._build_recursive(point_list[median + 1:], depth + 1)
             return node
         
         # 左だけ再帰する場合
         elif (len(left_list) > leaf_size) and (len(right_list) <= leaf_size):
             node = Node()
             node.location = point_list[median]
-            node.lhs = self.build(point_list[0: median], depth + 1)
+            node.lhs = self._build_recursive(point_list[0: median], depth + 1)
             return node
 
         # 右だけ再帰する場合
         elif (len(left_list) <= leaf_size) and (len(right_list) > leaf_size):
             node = Node()
             node.location = point_list[median]
-            node.rhs = self.build(point_list[median + 1:], depth + 1)
+            node.rhs = self._build_recursive(point_list[median + 1:], depth + 1)
             return node
 
 
@@ -61,6 +65,8 @@ if __name__ == '__main__':
     point_list = [[random()*10, random()*10] for _ in range(10)]
     print(point_list)
 
-    kdtree = kdtree()    
-    node = kdtree.build(point_list)
+    kdtree = kdtree(point_list)    
+    kdtree.build()
+
+    print(kdtree.node.location)
 
