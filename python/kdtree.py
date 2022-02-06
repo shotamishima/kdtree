f817e00dd9c140598b671397aa924236a6ba1cc3
shota-mishima <shota_mishima@toyota-tokyo.tech>
2022-02-06 10:48:18 +0900

    Add nnsearch

--- python/kdtree.py
+++ python/kdtree.py
@@ -1,16 +1,17 @@
+from email.errors import NonPrintableDefect
+import math
+from mimetypes import guess_all_extensions
 from random import random
+from tkinter import W
 
 
 class Node:
-    def location(element):
-        location = element
-
-    def lhs(point_list):
-        lhs = point_list
-    
-    def rhs(point_list):
-        rhs= point_list
-
+    def __init__(self):
+        self.location = None
+        self.lhs = None
+        self.rhs = None
+        self.axis = None
+        
 
 class kdtree:
 
@@ -19,9 +20,14 @@ class kdtree:
         self.node = Node()
 
     def build(self, depth=0, leaf_size=4):
-        self.node = self._build_recursive(self.point_list, depth, leaf_size)
+        self.node = self._build_recursive(self.point_list, depth)
 
-    def _build_recursive(self, point_list, depth=0, leaf_size=4):
+    def nnsearch(self, query):
+        min_dist = 100000 # TODO infinity
+        guess = self._nnsearch_recursive(self.node, query, min_dist)
+        return guess
+        
+    def _build_recursive(self, point_list, depth=0):
         if not point_list:
             return
 
@@ -33,32 +39,53 @@ class kdtree:
         # axisでソート
         point_list.sort(key=lambda x: x[axis])
         # 中央値となるインデックスを算出
-        median = len(point_list) %2
-        left_list = point_list[0: median]
-        right_list = point_list[median + 1:]
+        median = (len(point_list) - 1) // 2
 
-        # 左右とも再帰する場合
-        if (len(left_list) > leaf_size) and (len(right_list) > leaf_size):
-            node = Node()
-            node.location = point_list[median] # その深さにおけるノードになる要素
-            node.lhs= self._build_recursive(point_list[0: median], depth + 1)
-            node.rhs =self._build_recursive(point_list[median + 1:], depth + 1)
-            return node
+        print(axis)
+        print(median)
+        print(point_list)
+        print('-----------------')
+
+        node = Node()
+        node.location = point_list[median]
+        node.axis = axis
+        node.lhs = self._build_recursive(point_list[0: median], depth + 1)
+        node.rhs = self._build_recursive(point_list[median + 1:], depth + 1)
+
+        return node
+
+
+    def _nnsearch_recursive(self, node, query, min_dist):
+
+        current_point = node.location
+        dist = self._distance(current_point, query)
+        if dist < min_dist:
+            min_dist = dist
+            guess = current_point
         
-        # 左だけ再帰する場合
-        elif (len(left_list) > leaf_size) and (len(right_list) <= leaf_size):
-            node = Node()
-            node.location = point_list[median]
-            node.lhs = self._build_recursive(point_list[0: median], depth + 1)
-            return node
+        # 比較する軸を選択
+        axis = node.axis
+        if query[axis] < current_point[axis]:
+            next_node = node.lhs
+        else:
+            next_node = node.rhs
+        
+        guess = self._nnsearch_recursive(self, next_node, query, min_dist)
 
-        # 右だけ再帰する場合
-        elif (len(left_list) <= leaf_size) and (len(right_list) > leaf_size):
-            node = Node()
-            node.location = point_list[median]
-            node.rhs = self._build_recursive(point_list[median + 1:], depth + 1)
-            return node
+        return guess
 
+
+    def _distance(p1, p2):
+       """
+       p1, p2: list n-dimention
+       """ 
+       dist = 0
+       for element1, element2 in zip(p1, p2):
+           dist += (element1 - element2) * (element1 - element2)
+       return math.sqrt(dist)
+
+
+        
 
 if __name__ == '__main__':
 
@@ -69,4 +96,5 @@ if __name__ == '__main__':
     kdtree.build()
 
     print(kdtree.node.location)
+    print(kdtree.node.axis)
 
